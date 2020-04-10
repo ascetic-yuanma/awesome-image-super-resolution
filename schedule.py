11ee29e2e6ebe056e5cb340a91439741524576d0
yuan.ma <yuan.ma@whut.edu.cn>
2020-04-10 14:25:38 +0800

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -1,5 +1,4 @@
 import os
 
-os.system("python train.py -opt options/train/train_test.yml")
-
-os.system("python test.py -opt options/test/test_test.yml")
+os.system("python train.py -opt options/train/train_SRCNN.yml")
+os.system("python test.py -opt options/test/test_SRCNN.yml")
